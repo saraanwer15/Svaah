ecd862bc8f3ca4ec4a4c341f81e8782683b527d8
saraanwer15 <saraanwer13@gmail.com>
2020-09-22 02:50:51 +0530

    Welcome and timer

---
+++ Shvaas/Free_Trial_Page.swift
@@ -0,0 +1,16 @@
+//
+//  Free_Trial_Page.swift
+//  Shvaas
+//
+//  Created by user181178 on 9/22/20.
+//
+
+import Foundation
+import UIKit
+
+class Free_Trial_Page : UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
--- Shvaas/ViewController.swift
+++ Shvaas/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
+    
+    @IBOutlet weak var Status_of_recording: UILabel!
+    var recordingSession: AVAudioSession!
+    var audioRecorder: AVAudioRecorder!
+    var time_left = 30
+    var timer = Timer()
+    func stop(){
+        let Load_freetrial_page = storyboard?.instantiateViewController(identifier: "Trial_Page") as! Free_Trial_Page
+            self.present(Load_freetrial_page,animated: true)
     }
-
-
+        
+    @objc func start(){
+        time_left-=1
+        if(time_left<0){
+            stop()
+            timer.invalidate()
+        }
+        else{
+            print(time_left)
+        }
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        let timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(start), userInfo: nil, repeats: true )
+    }
+    
+    
 }
 
